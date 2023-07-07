d6b9aee3e0bf231520582f01c5dd2777bd9566a2
hkshakib <humayunkibriashakib@gmail.com>
2023-07-07 13:48:27 +0600

    fix: Skills Padding and margin

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -6,11 +6,15 @@ function Skills() {
   return (
     <div className='flex flex-col lg:flex-row'>
         
-        <div className='flex lg:flex-1 mr-[10px]'>
-            <img className='bg-contain' src={Image} alt="skills" />
+        <div className='flex lg:flex-1 justify-end mr-[10px]'>
+            <div className='flex basis-[90%]'>
+                <img className='bg-contain' src={Image} alt="skills" />
+            </div>
         </div>
 
-        <div className='flex flex-col lg:flex-1 lg:justify-start lg:items-start lg:pt-[50px] pl-4 lg:pl-12'>
+        <div className='flex flex-1 justify-end'>
+
+            <div className='flex basis-[90%] flex-col lg:justify-start lg:items-start lg:pt-[50px] pl-4 lg:pl-12'>      
             <h1 className='text-[56px] font-mono'>Skills</h1>
             <div className='flex flex-wrap justify-start items-center gap-2 lg:gap-2 mb-[10px]'>
                 <h2 className='text-blue-600 text-[14px] lg:text-[18px]'>Languages: </h2>
@@ -48,10 +52,10 @@ function Skills() {
                 <h3 className='text-gray-500 text-[12px] lg:text-[1rem]'>Celery Beat |</h3>
                 <h3 className='text-gray-500 text-[12px] lg:text-[1rem]'>React Query |</h3>
                 <h3 className='text-gray-500 text-[12px] lg:text-[1rem]'>Flutter |</h3>
-                <h3 className='text-gray-500 text-[12px] lg:text-[1rem]'>Web Scraping |</h3>
+                <h3 className='text-gray-500 text-[12px] lg:text-[1rem]'>Web Scraping</h3>
             </div>
 
-            <h1 className='lg:mt-[60px] text-[15px] lg:text-[20px]'>Proficient in:</h1>
+            <h1 className='pt-6 text-[15px] lg:text-[20px]'>Proficient in:</h1>
 
             <div className='flex flex-col'>
                 <div className='flex items-center'>
@@ -78,8 +82,9 @@ function Skills() {
                 <div className='flex items-center'>
                     <FaPooStorm/> <h3 className='text-gray-500 ml-4 text-[12px] lg:text-[18px]'>Data Structure and Algorithms</h3>
                 </div>
-
+                </div>
             </div>
+
         </div>
     </div>
   )
