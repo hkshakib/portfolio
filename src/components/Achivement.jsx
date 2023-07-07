a029debb58456e744abe5b33303e3a781c196bc4
hkshakib <humayunkibriashakib@gmail.com>
2023-07-07 23:39:30 +0600

    feat: Create a New component called Achivement and Dynamically Rendered it to Achivements page

---
+++ src/components/Achivement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AiOutlineFire } from 'react-icons/ai';
+
+function Achivement({Text}) {
+  return (
+    <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
+        <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'><AiOutlineFire className='text-red-700 text-[25px]'/> {Text} </h2>
+    </div>
+  )
+}
+
+export default Achivement;
--- src/pages/Achivements.jsx
+++ src/pages/Achivements.jsx
@@ -1,54 +1,32 @@
 import React from 'react';
-import { AiOutlineFire } from 'react-icons/ai';
-import Achivment from "../static/Achivment.jpg";
+import AchivmentImage from "../static/Achivment.jpg";
+import Achivement from '../components/Achivement';
 
 
-function Achivements() {
+const Achivements = () => {
   return (
     <div className='flex flex-col lg:flex-row'>
         
         <div className='flex lg:flex-1 m-[15px] lg:ml-[40px]'>
-            <img className='bg-contain rounded-lg' src={Achivment} alt="skills" />
+            <img className='bg-contain rounded-lg' src={AchivmentImage} alt="skills" />
         </div>
 
         <div className='flex flex-col lg:flex-1 lg:justify-start lg:items-start lg:pt-[50px] pl-4 lg:pl-12'>
+
             <h1 className='text-[35px] lg:text-[56px] font-mono'>Achivements</h1>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-                <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'><AiOutlineFire className='text-red-700 text-[25px]'/> Participated in ICPC Dhaka Regional 2021(BUBT). </h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> 8th Among Private University in ICPC Preliminary 2021.</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> Top 10 in NSU ICE Fest Inter University Programming Contest 2020</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/>Champions, Mathophobic Skyvers, LU Intra University Programming Competition 2020.</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px] '>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> Champions, LUZeroIQ, LU Intra University Programming Competition 2021.</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-             <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> Top 6 in Bangladesh, LUZeroIQ, IEEE Extreme 14.</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-             <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> Top 10 In Country Wise Ranking, Codeforces Round 643 (Div. 2) </h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-             <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> National ICT Quiz Champions 2018</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> LU Intra University ICT Quiz Champions 2018</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> LU Intra University ICT Quiz Champions 2019</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> LU Intra University ICT Quiz Champions 2020</h2>
-            </div>
-            <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-              <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono'> <AiOutlineFire className='text-red-700 text-[25px]'/> LU Intra University ICT Quiz Champions 2021</h2>
-            </div>
+            <Achivement Text={"Participated in ICPC Dhaka Regional 2021(BUBT)."}/>
+            <Achivement Text={"8th Among Private University in ICPC Preliminary 2021."}/>
+            <Achivement Text={"Top 10 in NSU ICE Fest Inter University Programming Contest 2020."}/>
+            <Achivement Text={"Champions, Mathophobic Skyvers, LU Intra University Programming Competition 2020."}/>
+            <Achivement Text={"Champions, LUZeroIQ, LU Intra University Programming Competition 2021."}/>
+            <Achivement Text={"Top 6 in Bangladesh, LUZeroIQ, IEEE Extreme 14."}/>
+            <Achivement Text={"Top 10 In Country Wise Ranking, Codeforces Round 643 (Div. 2)."}/>
+            <Achivement Text={"National ICT Quiz Champions 2018."}/>
+            <Achivement Text={"LU Intra University ICT Quiz Champions 2018."}/>
+            <Achivement Text={"LU Intra University ICT Quiz Champions 2019."}/>
+            <Achivement Text={"LU Intra University ICT Quiz Champions 2020."}/>
+            <Achivement Text={"LU Intra University ICT Quiz Champions 2021."}/>
+
         </div>
     </div>
   )
