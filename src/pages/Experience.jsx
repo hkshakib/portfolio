31218b2c2d782a2280d032327c73f8fe6fc1e45f
hkshakib <humayunkibriashakib@gmail.com>
2023-07-07 18:39:59 +0600

    fix: typo of className is resolve in Experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -4,12 +4,12 @@ function Experience() {
   return (
     <div className='flex flex-col justify-center items-center'>
         <h1 className='text-[35px] lg:text-[56px] font-mono mb-[50px]'>Experience</h1>
-        <div class="max-w-7xl mx-auto w-full grid grid-cols-9 px-2">
-            <div class="col-span-4 w-full h-full ">
-                <div class="w-full h-full bg-[#FCF4FF] rounded-md p-2 md:pl-4">
-                    <h1 class="text-black text-xl font-medium py-1">Junior Software Engineer (Part-Time)</h1>
-                    <h2 class="text-black text-[16px] font-normal py-1">March 2021 - January 2022</h2>
-                    <p class="text-gray-500 sm:text-sm text-xs">
+        <div className="max-w-7xl mx-auto w-full grid grid-cols-9 px-2">
+            <div className="col-span-4 w-full h-full ">
+                <div className="w-full h-full bg-[#FCF4FF] rounded-md p-2 md:pl-4">
+                    <h1 className="text-black text-xl font-medium py-1">Junior Software Engineer (Part-Time)</h1>
+                    <h2 className="text-black text-[16px] font-normal py-1">March 2021 - January 2022</h2>
+                    <p className="text-gray-500 sm:text-sm text-xs">
                         During my tenure at LIILAB, I worked as a backend developer for 8 months and contributed to the development of PCBuilder, 
                         a comprehensive full-stack website. PCBuilder is a user-friendly platform designed to assist individuals who lack knowledge about purchasing PCs. 
                         It enables users to easily navigate through the website, select desired PC components, and build their own customized PC based on their specific requirements. 
@@ -18,24 +18,24 @@ function Experience() {
                     </p>
                 </div>
             </div>
-            <div class="relative col-span-1 w-full h-full flex justify-center items-center">
-                <div class="h-full w-1 bg-indigo-300"></div>
-                <div class="absolute w-6 h-6 rounded-full bg-indigo-400 z-1 text-white text-center">1</div>
+            <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+                <div className="h-full w-1 bg-indigo-300"></div>
+                <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-1 text-white text-center">1</div>
             </div>
-            <div class="col-span-4 w-full h-full"></div>
+            <div className="col-span-4 w-full h-full"></div>
 
 
 
-            <div class="col-span-4 w-full h-full"></div>
-            <div class="relative col-span-1 w-full h-full flex justify-center items-center">
-                <div class="h-full w-1 bg-indigo-300"></div>
-                <div class="absolute w-6 h-6 rounded-full bg-indigo-400 z-1 text-white text-center">2</div>
+            <div className="col-span-4 w-full h-full"></div>
+            <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+                <div className="h-full w-1 bg-indigo-300"></div>
+                <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-1 text-white text-center">2</div>
             </div>
-            <div class="col-span-4 w-full h-full ">
-                <div class="w-full h-full bg-[#FEFAF0] rounded-md p-2 md:pl-4">
-                    <h1 class="text-black text-xl font-medium py-1">ACM Coordinator Lead</h1>
-                    <h2 class="text-black text-[16px] font-normal py-1">January 2021 - January 2022</h2>
-                    <p class="text-gray-500 sm:text-sm text-xs">
+            <div className="col-span-4 w-full h-full ">
+                <div className="w-full h-full bg-[#FEFAF0] rounded-md p-2 md:pl-4">
+                    <h1 className="text-black text-xl font-medium py-1">ACM Coordinator Lead</h1>
+                    <h2 className="text-black text-[16px] font-normal py-1">January 2021 - January 2022</h2>
+                    <p className="text-gray-500 sm:text-sm text-xs">
                         As the Lead ACM Co-Ordinator of the Leading University Computer Club, I took charge of organizing and overseeing a diverse range of events, including programming contests, 
                         quiz contests, and ICT fests. With my strong leadership skills, I led a dedicated team of organizers to successfully execute these events, ensuring their smooth operation and 
                         high-quality standards. Through these initiatives, I provided an enriching platform for students to showcase their programming prowess, engage in healthy competition, 
@@ -45,11 +45,11 @@ function Experience() {
                 </div>
             </div>
 
-            <div class="col-span-4 w-full h-full ">
-                <div class="w-full h-full bg-[#FFF4F4] rounded-md p-2 md:pl-4">
-                    <h1 class="text-black text-xl font-medium py-1">ACM Cordinator</h1>
-                    <h2 class="text-black text-[16px] font-normal py-1">January 2020 - January 2021</h2>
-                    <p class="text-gray-500 sm:text-sm text-xs">
+            <div className="col-span-4 w-full h-full ">
+                <div className="w-full h-full bg-[#FFF4F4] rounded-md p-2 md:pl-4">
+                    <h1 className="text-black text-xl font-medium py-1">ACM Cordinator</h1>
+                    <h2 className="text-black text-[16px] font-normal py-1">January 2020 - January 2021</h2>
+                    <p className="text-gray-500 sm:text-sm text-xs">
                         As the ACM Co-Ordinator of the Leading University Computer Club for a period of 1 year, 
                         I played a crucial role in organizing and managing various programming contests, quiz contests, and ICT fests. 
                         I successfully collaborated with a team of talented individuals to create engaging and challenging events that fostered a competitive spirit among participants. 
@@ -60,11 +60,11 @@ function Experience() {
                     </p>
                 </div>
             </div>
-            <div class="relative col-span-1 w-full h-full flex justify-center items-center">
-                <div class="h-full w-1 bg-indigo-300"></div>
-                <div class="absolute w-6 h-6 rounded-full bg-indigo-400 z-1 text-white text-center">3</div>
+            <div className="relative col-span-1 w-full h-full flex justify-center items-center">
+                <div className="h-full w-1 bg-indigo-300"></div>
+                <div className="absolute w-6 h-6 rounded-full bg-indigo-400 z-1 text-white text-center">3</div>
             </div>
-            <div class="col-span-4 w-full h-full"></div>
+            <div className="col-span-4 w-full h-full"></div>
         </div>
     </div>
   )
