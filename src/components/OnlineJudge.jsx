38afd3f9da34823166259c8624764cb10cf77a30
hkshakib <humayunkibriashakib@gmail.com>
2023-07-07 23:32:00 +0600

    feat: Added Dynamically Add Problemsolving Card named: OnlineJudge

---
+++ src/components/OnlineJudge.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const OnlineJudge = ({Name, Designation, RatingsOrMaxStreak, Text, Problems, Handle, Url, PhotoUrl}) => {
+  return (
+    <div className='flex flex-1 flex-col'>
+            <div className='flex justify-center items-center'>
+                <img className='h-[50px] w-[50px] lg:h-[100px] lg:w-[100px]' src={PhotoUrl} alt="" />
+                <h1 className='lg:ml-[10px] lg:pt-[30px] text-[20px] lg:text-[30px] font-mono'>
+                 {Name}
+               </h1>
+            </div>
+
+            <div className='flex justify-around items-center pt-10 pr-2 pl-2 lg:pl-0 lg:pr-0 '>
+                <div className='flex flex-col items-center bg-white h-[100px] w-[150px] rounded-lg border'>
+                    <h1 className='uppercse font-mono lg:text-[16px] text-[12px] mb-[10px] lg:p-2 p-2'>{Designation}</h1>
+                    <h2 className='text-[10px] lg:text-[12px] font-mono text-[#4E4FEB] p-2'>{Text} {RatingsOrMaxStreak}</h2>
+                </div>
+                <div className='flex flex-col items-center bg-white h-[100px] w-[150px] rounded-lg border ml-2 lg:ml-0'>
+                    <h1 className='uppercse font-mono lg:text-[16px] text-[12px] mb-[10px] lg:p-2 p-2'>Total Solved</h1>
+                    <h2 className='text-[10px] lg:text-[12px] font-mono text-[#4E4FEB] p-2'>{Problems} Problems</h2>
+                </div>
+            </div>
+
+            <div className='flex flex-1 lg:flex-0 justify-center items-end p-4'>
+                <button className='flex justify-center items-center border w-[100px] h-[30px] bg-blue-800 rounded-md text-white'>
+                    <a className='text-[12px]' href={Url}>{Handle}</a>
+                </button>
+            </div>
+        </div>
+  )
+}
+
+export default OnlineJudge;
--- src/pages/ProblemSolving.jsx
+++ src/pages/ProblemSolving.jsx
@@ -12,7 +12,6 @@ const ProblemSolving = () => {
         <div className='flex gap-12 justify-center lg:gap-20 flex-wrap lg:p-10'>
 
             <div className='flex lg:flex-1 bg-[#FCF4FF] h-[250px] lg:h-[400px] lg:w-[100%] shadow-lg border rounded-md'>
-
                 <div className='flex flex-1 flex-col'>
 
                     <div className='flex justify-center items-center'>
@@ -43,11 +42,9 @@ const ProblemSolving = () => {
                     </div>
 
                 </div>
-
             </div>
 
             <div className='flex lg:flex-1 bg-[#FEFAF0] h-[250px] lg:h-[400px] lg:w-[400px] shadow-lg border rounded-md'>
-
                 <div className='flex flex-1 flex-col'>
 
                         <div className='flex justify-center items-center'>
@@ -69,42 +66,42 @@ const ProblemSolving = () => {
                             </div>
 
                         </div>
+
                         <div className='flex flex-1 lg:flex-0 justify-center items-end p-4'>
                             <button className='flex justify-center items-center border w-[100px] h-[30px] bg-blue-800 rounded-md text-white'>
                                 <a className='text-[12px]' href="https://www.codechef.com/users/hkshakib">hkshakib</a>
                             </button>
                         </div>
-                    </div>
+
                 </div>
+            </div>
 
-                <div className='flex lg:flex-1 bg-[#FFF4F4] h-[250px] lg:h-[400px] lg:w-[400px] shadow-lg border rounded-md'>
-                    <div className='flex flex-1 flex-col'>
-                        <div className='flex justify-center items-center'>
-                            <img className='h-[50px] w-[50px] lg:h-[100px] lg:w-[100px] pt-4' src={LC} alt="" />
-                            <h1 className='lg:ml-[10px] lg:pt-[30px] text-[20px] lg:text-[30px] font-mono'>
-                                LeetCode
-                            </h1>
-                        </div>
-                        <div className='flex justify-around items-center pt-10 pr-2 pl-2 lg:pl-0 lg:pr-0'>
-
+            <div className='flex lg:flex-1 bg-[#FFF4F4] h-[250px] lg:h-[400px] lg:w-[400px] shadow-lg border rounded-md'>
+                <div className='flex flex-1 flex-col'>
+                    <div className='flex justify-center items-center'>
+                        <img className='h-[50px] w-[50px] lg:h-[100px] lg:w-[100px] pt-4' src={LC} alt="" />
+                        <h1 className='lg:ml-[10px] lg:pt-[30px] text-[20px] lg:text-[30px] font-mono'>
+                             LeetCode
+                        </h1>
+                    </div>
+                    <div className='flex justify-around items-center pt-10 pr-2 pl-2 lg:pl-0 lg:pr-0'>
                             <div className='flex flex-col items-center bg-white h-[100px] w-[150px] rounded-lg border'>
                                 <h1 className='uppercse font-mono lg:text-[16px] text-[12px] mb-[10px] lg:p-2 p-2'>Max Streak</h1>
                                 <h2 className='text-[10px] lg:text-[12px] font-mono text-[#4E4FEB] p-2'>35</h2>
                             </div>
-
                             <div className='flex flex-col items-center bg-white h-[100px] w-[150px] rounded-lg border ml-2 lg:ml-0'>
                                 <h1 className='uppercse font-mono lg:text-[16px] text-[12px] mb-[10px] lg:p-2 p-2'>Total Solved</h1>
                                 <h2 className='text-[10px] lg:text-[12px] font-mono text-[#4E4FEB] p-2'>113 Problems</h2>
-                            </div>
-                            
-                        </div>
-                        <div className='flex flex-1 lg:flex-0 justify-center items-end p-4'>
-                            <button className='flex justify-center items-center border w-[100px] h-[30px] bg-blue-800 rounded-md text-white'>
-                                <a className='text-[12px]' href="https://leetcode.com/humayun_kibria/">humayun_kibria</a>
-                            </button>
-                        </div>
+                            </div>    
+                    </div>
+                    <div className='flex flex-1 lg:flex-0 justify-center items-end p-4'>
+                        <button className='flex justify-center items-center border w-[100px] h-[30px] bg-blue-800 rounded-md text-white'>
+                            <a className='text-[12px]' href="https://leetcode.com/humayun_kibria/">humayun_kibria</a>
+                        </button>
                     </div>
                 </div>
+            </div>
+
         </div>
     </div>
   )
