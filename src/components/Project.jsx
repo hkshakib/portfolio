a4a1b294e6c4986e1ff81a9873ae466c375e6eb5
hkshakib <humayunkibriashakib@gmail.com>
2023-07-12 20:17:10 +0600

    feat: Added Dynamically import Project card using two seperate components named: Project and ProjectWithCode

---
+++ src/components/Project.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Project = ({Title, Summery, Technologies, CodeUrl}) => {
+  return (
+    <div className='flex flex-col items-center  bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
+        <div className='flex justify-center lg:text-[18px] font-mono p-1 lg:pt-2 font-bold'>
+            <span>{Title}</span>
+        </div>
+        <div className='flex flex-col'>
+            <span className='text-[14px] p-2 lg:p-4 text-gray-500'>
+                {Summery}
+            </span>
+            <span className='text-[14px] lg:text[16px] pl-2 lg:pl-4 font-bold lg:pt-1'>
+                Used Technology:
+            </span>
+            {Technologies.map((tech) =>
+                <span className='text-[12px] font-mono pl-2 lg:pl-4'># {tech}</span>
+            )}
+        </div>
+        <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
+            <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
+                <a className='font-mono' href={CodeUrl}>Code</a>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Project;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ProjectWithLive from '../components/ProjectWithLive';
+import Project from '../components/Project';
+
+          // <Project 
+          //   Title={"StoreFront (Backend)"}
+          //   Summery={""}
+          //   Technologies={
+          //     ["Django", "Django Rest", "Djoser", "django Filters", "Session Authentication"]
+          //   }
+          //   CodeUrl={""}
+          // />
 
 const Projects = () => {
   return (
@@ -10,143 +21,69 @@ const Projects = () => {
 
       <div className='flex flex-wrap gap-8 lg:p-12 justify-center'>
 
-          <div className='flex flex-col justify-center items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-2'>React Fun Box</sapn>
+          <ProjectWithLive/>
+          <Project 
+            Title={"Personal Protfolio"}
+            Summery={"I designed and built a visually appealing and responsive portfolio website using React and Tailwind CSS. The website consists of a single page with distinct sections, such as Home, Skills, Problem Solving, Education, Achivements, and Contact."}
+            Technologies={
+              ["React", "React Router Dom", "Tailwind CSS"]
+            }
+            CodeUrl={"https://github.com/hkshakib/protfolio"}
+          />
 
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                This Project contains a collection of React apps developed as part of the React Fun Box project. 
-                Each app focuses on a different concept or game, providing interactive and educational experiences.
-              </p>
-            </div>
+          <Project 
+            Title={"StoreFront (Backend)"}
+            Summery={"I have developed a complete E-commerce backend system that includes user registration, login functionality, product browsing, cart management, and a customizable admin panel."}
+            Technologies={
+              ["Django", "Django Rest", "Djoser", "django Filters", "Session Authentication"]
+            }
+            CodeUrl={"https://github.com/hkshakib/storefront2"}
+          />
+          <Project 
+            Title={"Django Authentication (Backend)"}
+            Summery={"This is the complete package of Django authentication system using Django Simple JWT.I implemented it using Django REST framework. It has several Endpoints for user including login, logout, signup and user profile."}
+            Technologies={
+              ["Django", "Django Rest", "Django Simple-JWT"]
+            }
+            CodeUrl={"https://github.com/hkshakib/DjangoAuthentication"}
+          />
+          <Project 
+            Title={"BID Buddy (Backend)"}
+            Summery={"BID BUDDY Project is a web application that enables users to participate in auctions for various items. It provides features such as user registration and authentication, an auction gallery to browse available items, the ability to place bids on items."}
+            Technologies={
+              ["React", "CSS", "Tailwind CSS", "React Router Dom", "Custom Hooks"]
+            }
+            CodeUrl={"https://github.com/hkshakib/auctionfrontend"}
+          />
+          <Project 
+            Title={"BID Buddy (Backend)"}
+            Summery={"BID BUDDY Project is a web application that enables users to participate in auctions for various items. It provides features such as user registration and authentication, an auction gallery to browse available items, the ability to place bids on items. Also, Included some endpoints for the Admin dashboard and statistics that not includes in frontend yet"}
+            Technologies={
+              ["Django", "Django Rest", "Django Simple-JWT"]
+            }
+            CodeUrl={"https://github.com/hkshakib/auctionbackend"}
+          />
 
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/reactfunbox">Code</a>
-              </button>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'>
-                <a className='font-mono' href="https://reactfunbox.vercel.app/">Live</a>
-              </button>
-            </div>
-          </div>
+          <Project 
+            Title={"Steller (FullStack)"}
+            Summery={"An Real Estate App Using Django REST, React And Redux. I implement REST, React Redux in this project while Learning Redux from Scratch."}
+            Technologies={
+              ["Django", "Django Rest", "React Js", "React Router", "Custom Hooks"]
+            }
+            CodeUrl={"https://github.com/hkshakib/Stellar"}
+          />
 
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>Personal Protfolio</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                I designed and built a visually appealing and responsive portfolio website using React and Tailwind CSS. 
-                The website consists of a single page with distinct sections, such as 
-                Home, Skills, Problem Solving, Education, Achivements, and Contact. 
-                It effectively showcases my skills, education, and achievements in a concise and organized manner. 
-                The website's responsive design ensures optimal viewing experience across various devices.
-              </p>
-            </div>
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/protfolio">Code</a>
-              </button>
-            </div>
-          </div>
-
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>StoreFront (Backend)</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                I have developed a complete E-commerce backend system that includes user registration, 
-                login functionality, product browsing, cart management, and a customizable admin panel. Users can register, 
-                log in, and browse through a wide range of products. They can add products to their cart, 
-                apply filters for a refined search, and access their profiles. The system is designed for future payment integration to provide a seamless purchasing experience.
-              </p>
-            </div>
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/storefront2">Code</a>
-              </button>
-            </div>
-          </div>
-
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>Django Authentication (Backend)</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                This is the complete package of Django authentication system using Django Simple JWT.
-                I implemented it using Django REST framework. It has several Endpoints for user including login, logout, signup and user profile.
-              </p>
-            </div>
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/DjangoAuthentication">Code</a>
-              </button>
-            </div>
-          </div>
-
-
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>BID Buddy (Frontend)</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                BID BUDDY Project is a web application that enables users to participate in auctions for various items. 
-                It provides features such as user registration and authentication, an auction gallery to browse available items, 
-                the ability to place bids on items.
-              </p>
-            </div>
-
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/auctionfrontend">Code</a>
-              </button>
-            </div>
-          </div>
-
-
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>BID Buddy (Backend)</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-              BID BUDDY Project is a web application that enables users to participate in auctions for various items. 
-              It provides features such as user registration and authentication, an auction gallery to browse available items, the ability to place bids on items. 
-              Also, Included some endpoints for the Admin dashboard and statistics that not includes in frontend yet
-              </p>
-            </div>
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/auctionbackend">Code</a>
-              </button>
-            </div>
-          </div>
-
-
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>Steller (FullStack)</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                An Real Estate App Using Django REST, React And Redux.
-                I implement REST, React Redux in this project while Learning Redux from Scratch.
-              </p>
-            </div>
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/Stellar">Code</a>
-              </button>
-            </div>
-          </div>
-
-          <div className='flex flex-col items-center bg-[#FEFAF0] border h-[400px] w-[300px] lg:w-[400px] shadow-lg'>
-            <sapn className='text-[18px] font-mono p-1'>Blog (Frontend)</sapn>
-            <div className='flex'>
-              <p className='text-[14px] p-4 text-gray-500'>
-                I implement this blog using React while Learning React.
-              </p>
-            </div>
-            <div className='flex flex-1 justify-center items-end pb-3 gap-4'>
-              <button className='border bg-white h-[40px] w-[100px] rounded hover:shadow-md'> 
-                <a className='font-mono' href="https://github.com/hkshakib/blogfrontend">Code</a>
-              </button>
-            </div>
-          </div>
-
+          <Project 
+            Title={"Blog (Frontend)"}
+            Summery={"I implement this blog using React while Learning React."}
+            Technologies={
+              ["React Js", "React Router Dom"]
+            }
+            CodeUrl={"https://github.com/hkshakib/blogfrontend"}
+          />
 
       </div>
+
     </div>
   )
 }
