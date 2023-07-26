02bebee841b4837d93e4984d2e8990f9cab58525
hkshakib <humayunkibriashakib@gmail.com>
2023-07-26 15:45:48 +0600

    feat: Added Scroll on top feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Skills from "./pages/Skills";
@@ -9,13 +9,30 @@ import Footer from "./components/Footer";
 import Experience from "./pages/Experience";
 import Achivements from "./pages/Achivements";
 import Research from "./pages/Research";
+import { FaArrowUp } from 'react-icons/fa';
+import { animateScroll as scroll } from 'react-scroll';
 
 const App = () => {
+
+  const [isAtTop, setIsAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsAtTop(window.scrollY === 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div className="w-full-screen bg-white font-inter flex flex-col gap-4">
       
       <Navbar />
+      
 
       <section id="home" className="flex pt-[50px] items-center justify-center relative">
         <Home />
@@ -52,6 +69,16 @@ const App = () => {
       <section id="footer" className="flex pt-[50px] lg:pt-[200px] items-center justify-center relative">
         <Footer/>
       </section>
+      <div
+        className={`fixed bottom-8 right-8 bg-gray-500 p-2 rounded-full text-white cursor-pointer ${
+          isAtTop ? "hidden" : ""
+        }`}
+        onClick={() =>
+          scroll.scrollToTop({ duration: 800, smooth: "easeInOutQuart" })
+        }
+      >
+        {<FaArrowUp size={24} />}
+      </div>
     </div>
   );
 };
