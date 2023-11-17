ba01269686d02a8ccaaa0520b81799ec8bf8d895
hkshakib <humayunkibriashakib@gmail.com>
2023-11-17 19:52:20 +0600

    style: apply apply derivatives on skills section

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -29,82 +29,46 @@ const Skills = () => {
         <div className="flex lg:basis-[90%] flex-col lg:justify-start lg:items-start lg:pt-[50px] pl-4 lg:pl-12">
           <h1 className="text-[56px] font-mono">Skills</h1>
           <div className="flex flex-wrap justify-start items-center gap-2 lg:gap-2 mb-[10px]">
-            <h2 className="text-blue-600 text-[14px] lg:text-[18px]">
-              Languages:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">C++ |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Python |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">C |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              JavaScript |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">JAVA |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">Dart</h3>
+            <h2 className="skillsDesc">Languages:</h2>
+            <h3 className="skills">C++ |</h3>
+            <h3 className="skills">Python |</h3>
+            <h3 className="skills">C |</h3>
+            <h3 className="skills">JavaScript |</h3>
+            <h3 className="skills">JAVA |</h3>
+            <h3 className="skills">Dart</h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]">
-            <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
-              Frontend:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">HTML |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">CSS |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              React JS |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Tailwind CSS |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">MUI |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Chakra UI |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Framer Motion
-            </h3>
+          <div className="skillsTitle">
+            <h2 className="skillsDesc">Frontend:</h2>
+            <h3 className="skills">HTML |</h3>
+            <h3 className="skills">CSS |</h3>
+            <h3 className="skills">React JS |</h3>
+            <h3 className="skills">Tailwind CSS |</h3>
+            <h3 className="skills">MUI |</h3>
+            <h3 className="skills">Chakra UI |</h3>
+            <h3 className="skills">Framer Motion</h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]">
-            <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
-              Backend:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Django |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Django REST |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              FastAPI
-            </h3>
+          <div className="skillsTitle">
+            <h2 className="skillsDesc">Backend:</h2>
+            <h3 className="eskills">Django |</h3>
+            <h3 className="skills">Django REST |</h3>
+            <h3 className="skills">FastAPI</h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]">
-            <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
-              SQL DB:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">MYSQL</h3>
+          <div className="skillsTitle">
+            <h2 className="skillsDesc">SQL DB:</h2>
+            <h3 className="skills">MYSQL</h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]">
-            <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
-              NoSQL DB:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Firebase |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Supabase
-            </h3>
+          <div className="skillsTitle">
+            <h2 className="skillsDesc">NoSQL DB:</h2>
+            <h3 className="skills">Firebase |</h3>
+            <h3 className="skills">Supabase</h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px] ">
-            <h2 className="text-blue-500 text-[14px] lg:text-[1rem]">
-              DevOps:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">Git |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Github |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">Docker</h3>
+          <div className="skillsTitle">
+            <h2 className="skillsDesc">DevOps:</h2>
+            <h3 className="skills">Git |</h3>
+            <h3 className="skills">Github |</h3>
+            <h3 className="skills">Docker</h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]">
+          <div className="skillsTitle">
             <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
               Fimiliar With:
             </h2>
@@ -130,72 +94,47 @@ const Skills = () => {
               Web Scraping
             </h3>
           </div>
-          <div className="flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]">
-            <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
-              Machine Learning:
-            </h2>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">NLP |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">TTS |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">VITS |</h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              FastPitch |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Tecotron |
-            </h3>
-            <h3 className="text-gray-500 text-[12px] lg:text-[1rem]">
-              Data Processing
-            </h3>
+          <div className="skillsTitle">
+            <h2 className="skillsDesc">Machine Learning:</h2>
+            <h3 className="skills">NLP |</h3>
+            <h3 className="skills">TTS |</h3>
+            <h3 className="skills">VITS |</h3>
+            <h3 className="skills">FastPitch |</h3>
+            <h3 className="skills">Tecotron |</h3>
+            <h3 className="skills">Data Processing</h3>
           </div>
 
           <h1 className="pt-6 text-[15px] lg:text-[20px]">Proficient in:</h1>
 
           <div className="flex flex-col">
             <div className="flex items-center">
-              <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
-                C++
-              </h3>
+              <FaPooStorm /> <h3 className="profficientSkills">C++</h3>
+            </div>
+            <div className="flex items-center">
+              <FaPooStorm /> <h3 className="profficientSkills">Python</h3>
+            </div>
+            <div className="flex items-center">
+              <FaPooStorm /> <h3 className="profficientSkills">JavaScript</h3>
             </div>
             <div className="flex items-center">
               <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
-                Python
-              </h3>
-            </div>
-            <div className="flex items-center">
-              <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
-                JavaScript
-              </h3>
-            </div>
-            <div className="flex items-center">
-              <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
+              <h3 className="profficientSkills">
                 Algorithomic Problem Solving
               </h3>
             </div>
             <div className="flex items-center">
-              <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
-                Backend
-              </h3>
+              <FaPooStorm /> <h3 className="profficientSkills">Backend</h3>
+            </div>
+            <div className="flex items-center">
+              <FaPooStorm /> <h3 className="profficientSkills">Frontend</h3>
             </div>
             <div className="flex items-center">
               <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
-                Frontend
-              </h3>
+              <h3 className="profficientSkills">Competitive Programming</h3>
             </div>
             <div className="flex items-center">
               <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
-                Competitive Programming
-              </h3>
-            </div>
-            <div className="flex items-center">
-              <FaPooStorm />{" "}
-              <h3 className="text-gray-500 ml-4 text-[12px] lg:text-[18px]">
+              <h3 className="profficientSkills">
                 Data Structure and Algorithms
               </h3>
             </div>
