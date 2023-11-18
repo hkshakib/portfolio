0556f11e78e569010b05e15518dd12784f113e7d
hkshakib <humayunkibriashakib@gmail.com>
2023-11-18 10:28:36 +0600

    feat: Dynamically import Skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,19 +1,10 @@
-import React from "react";
 import { motion } from "framer-motion";
+import React from "react";
 
-import Image from "../static/skills.jpg";
 import { FaPooStorm } from "react-icons/fa";
+import Image from "../static/skills.jpg";
 
-const skillsData = [
-  "C++",
-  "Python",
-  "JavaScript",
-  "Algorithomic Problem Solving",
-  "Backend",
-  "Frontend",
-  "Competitive Programming",
-  "Data Structure and Algorithms",
-];
+import { skills, skillsData } from "../static/Data";
 
 const Skills = () => {
   return (
@@ -39,67 +30,16 @@ const Skills = () => {
       >
         <div className="flex lg:basis-[90%] flex-col lg:justify-start lg:items-start lg:pt-[50px] pl-4 lg:pl-12">
           <h1 className="text-[56px] font-mono">Skills</h1>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">Languages:</h2>
-            <h3 className="skills">C++ |</h3>
-            <h3 className="skills">Python |</h3>
-            <h3 className="skills">C |</h3>
-            <h3 className="skills">JavaScript |</h3>
-            <h3 className="skills">JAVA |</h3>
-            <h3 className="skills">Dart</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">Frontend:</h2>
-            <h3 className="skills">HTML |</h3>
-            <h3 className="skills">CSS |</h3>
-            <h3 className="skills">React JS |</h3>
-            <h3 className="skills">Tailwind CSS |</h3>
-            <h3 className="skills">MUI |</h3>
-            <h3 className="skills">Chakra UI |</h3>
-            <h3 className="skills">Framer Motion</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">Backend:</h2>
-            <h3 className="eskills">Django |</h3>
-            <h3 className="skills">Django REST |</h3>
-            <h3 className="skills">FastAPI</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">SQL DB:</h2>
-            <h3 className="skills">MYSQL</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">NoSQL DB:</h2>
-            <h3 className="skills">Firebase |</h3>
-            <h3 className="skills">Supabase</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">DevOps:</h2>
-            <h3 className="skills">Git |</h3>
-            <h3 className="skills">Github |</h3>
-            <h3 className="skills">Docker</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="text-blue-500 text-[14px] lg:text-[18px]">
-              Fimiliar With:
-            </h2>
-            <h3 className="fimilarSkills">Design Pattern |</h3>
-            <h3 className="fimilarSkills">Redux |</h3>
-            <h3 className="fimilarSkills">Celery |</h3>
-            <h3 className="fimilarSkills">Celery Beat |</h3>
-            <h3 className="fimilarSkills">React Query |</h3>
-            <h3 className="fimilarSkills">Flutter |</h3>
-            <h3 className="fimilarSkills">Web Scraping</h3>
-          </div>
-          <div className="skillsTitle">
-            <h2 className="skillsDesc">Machine Learning:</h2>
-            <h3 className="skills">NLP |</h3>
-            <h3 className="skills">TTS |</h3>
-            <h3 className="skills">VITS |</h3>
-            <h3 className="skills">FastPitch |</h3>
-            <h3 className="skills">Tecotron |</h3>
-            <h3 className="skills">Data Processing</h3>
-          </div>
+          {skills.map((category, index) => (
+            <div className="skillsTitle" key={index}>
+              <h2 className="skillsDesc">{category.category}:</h2>
+              {category.skills.map((skill, skillIndex) => (
+                <h3 className="skills" key={skillIndex}>
+                  {skill} {skillIndex < category.skills.length - 1 && "|"}
+                </h3>
+              ))}
+            </div>
+          ))}
 
           <h1 className="pt-6 text-[15px] lg:text-[20px]">Proficient in:</h1>
           <div className="flex flex-col">
---
+++ src/static/Data.jsx
@@ -0,0 +1,61 @@
+export const skillsData = [
+  "C++",
+  "Python",
+  "JavaScript",
+  "Algorithomic Problem Solving",
+  "Backend",
+  "Frontend",
+  "Competitive Programming",
+  "Data Structure and Algorithms",
+];
+
+export const skills = [
+  {
+    category: "Languages",
+    skills: ["C++", "Python", "C", "JavaScript", "JAVA", "Dart"],
+  },
+  {
+    category: "Frontend",
+    skills: [
+      "HTML",
+      "CSS",
+      "React JS",
+      "Tailwind CSS",
+      "MUI",
+      "Chakra UI",
+      "Framer Motion",
+    ],
+  },
+  {
+    category: "Backend",
+    skills: ["Django", "Django REST", "FastAPI"],
+  },
+  {
+    category: "SQL DB",
+    skills: ["MYSQL"],
+  },
+  {
+    category: "NoSQL DB",
+    skills: ["Firebase", "Supabase"],
+  },
+  {
+    category: "DevOps",
+    skills: ["Git", "Github", "Docker"],
+  },
+  {
+    category: "Fimiliar With",
+    skills: [
+      "Design Pattern",
+      "Redux",
+      "Celery",
+      "Celery Beat",
+      "React Query",
+      "Flutter",
+      "Web Scraping",
+    ],
+  },
+  {
+    category: "Machine Learning",
+    skills: ["NLP", "TTS", "VITS", "FastPitch", "Tecotron", "Data Processing"],
+  },
+];
