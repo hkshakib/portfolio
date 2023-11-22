7e7ba72871f80c18452cfb200bea0d703a37a4c2
hkshakib <humayunkibriashakib@gmail.com>
2023-11-22 15:52:20 +0600

    feat: blog content structure added

--- src/blog/Blogs.jsx
+++ src/blog/Blogs.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-
+import Content from "./components/Content";
 const Blogs = () => {
   return (
     <div className="flex flex-col w-full h-[100vh]">
-      <Navbar/>
+      <Navbar />
+      <Content />
     </div>
   );
 };
---
+++ src/blog/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="flex flex-1">
+      <div className="flex basis-[80%] border p-5">Here is the blog content</div>
+      <div className="flex flex-1 justify-center pt-5">
+        <h3>Recently Added</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
