59f6e50c16ce2e31cccf9bad5662669ba1e87de8
hkshakib <humayunkibriashakib@gmail.com>
2023-11-26 12:52:27 +0600

    feat: Added blog content design

--- src/blog/components/Content.jsx
+++ src/blog/components/Content.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 const Content = () => {
   return (
-    <div className="flex flex-1">
-      <div className="flex basis-[80%] border p-5">Here is the blog content</div>
+    <div className="flex flex-1 p-1">
+      <div className="flex flex-col basis-[80%] border border-r-1 p-10">
+        <div className="">Slider</div>
+      </div>
       <div className="flex flex-1 justify-center pt-5">
         <h3>Recently Added</h3>
       </div>
