b145022de28de601a891a91580451b0b22cd8943
hkshakib <humayunkibriashakib@gmail.com>
2024-05-25 12:44:20 +0600

    fix: Modal Content typography

--- src/blog/components/BlogModal.jsx
+++ src/blog/components/BlogModal.jsx
@@ -16,7 +16,7 @@ const BlogModal = ({ blog, onClose }) => {
       onClick={onClose}
     >
       <div
-        className="bg-white p-8 rounded-md shadow-lg h-[90vh] overflow-y-auto w-[90vw] md:w-[90vw] lg:w-[90vw] relative"
+        className="bg-white p-8 rounded-md shadow-lg h-[90vh] overflow-y-auto w-[90vw] md:w-[90vw] lg:w-[60vw] relative"
         onClick={handleModalClick}
       >
         
@@ -26,16 +26,18 @@ const BlogModal = ({ blog, onClose }) => {
         >
           <ImCross className="text-[20px]"/>
         </button>
-        <div className="sticky top-0 text-center md:text-[20px] lg:text-[30px] font-bold mb-4 z-[9999]">{blog.title}</div>
+        <div className="text-center md:text-[20px] lg:text-[30px] font-bold mb-4 z-[9999]">{blog.title}</div>
         <div className="flex justify-center">
             <img src={blog.photo} alt={blog.title} className="w-[50%] h-[50vh] mb-4 bg-cover" />
 
 
         </div>
-        <div className="text-gray-700 mb-4 p-10">
+        <div className="flex justify-center text-gray-700 mb-4 p-10">
           {blog.author} - {blog.date}
         </div>
-        <p className="flex flex-wrap text-gray-700 text-[16px] p-20">{blog.content}</p>
+        <div className="flex justify-center">
+          <article className="text-[16px] prose lg:prose-xl">{blog.content}</article>
+        </div>
       </div>
 
     </div>,
