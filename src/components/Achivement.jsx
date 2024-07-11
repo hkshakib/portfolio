b2404a0c21eafddea49bcb2c819994ec870a6bea
hkshakib <humayunkibriashakib@gmail.com>
2024-07-12 00:27:51 +0600

    refractor: refractor achivement

--- src/components/Achivement.jsx
+++ src/components/Achivement.jsx
@@ -3,7 +3,9 @@ import { AiOutlineFire } from 'react-icons/ai';
 function Achivement({Text}) {
   return (
     <div className='flex flex-wrap justify-start items-center gap-1 lg:gap-2 mb-[10px]'>
-        <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono text-[#090E34]'><AiOutlineFire className='text-red-700 text-[25px]'/> {Text} </h2>
+        <h2 className='flex gap-1 items-center text-[14px] lg:text-[18px] font-mono text-[#090E34]'>
+          <AiOutlineFire className='text-red-700 text-[25px]'/> {Text} 
+        </h2>
     </div>
   )
 }
--- src/pages/Achivements.jsx
+++ src/pages/Achivements.jsx
@@ -4,6 +4,26 @@ import AchivmentImage from "../static/Achivment.jpg";
 import Achivement from "../components/Achivement";
 
 const Achivements = () => {
+  const achievementsData = [
+    "Participated in ICPC Dhaka Regional 2021 (BUBT).",
+    "8th Among Private Universities in ICPC Preliminary 2021.",
+    "Top 10 in NSU ICE Fest Inter University Programming Contest 2020.",
+    "Champions, Mathophobic Skyvers, LU Intra University Programming Competition 2020.",
+    "Champions, LUZeroIQ, LU Intra University Programming Competition 2021.",
+    "Top 6 in Bangladesh, LUZeroIQ, IEEE Extreme 14.",
+    "Top 10 In Country Wise Ranking, Codeforces Round 643 (Div. 2).",
+    "National ICT Quiz Champions 2018.",
+    "LU Intra University ICT Quiz Champions 2018.",
+    "LU Intra University ICT Quiz Champions 2019.",
+    "LU Intra University ICT Quiz Champions 2020.",
+    "LU Intra University ICT Quiz Champions 2021.",
+  ];
+  const renderAchievements = (achievements) => {
+    return achievements.map((text, index) => (
+      <Achivement key={index} Text={text} />
+    ));
+  };
+
   return (
     <section
       id="achivements"
@@ -32,40 +52,7 @@ const Achivements = () => {
           className="flex flex-col lg:flex-1 lg:justify-start lg:items-start lg:pt-[50px] pl-4 lg:pl-12"
         >
           <h1 className="text-[35px] lg:text-[56px] font-mono">Achivements</h1>
-          <Achivement
-            Text={"Participated in ICPC Dhaka Regional 2021(BUBT)."}
-          />
-          <Achivement
-            Text={"8th Among Private University in ICPC Preliminary 2021."}
-          />
-          <Achivement
-            Text={
-              "Top 10 in NSU ICE Fest Inter University Programming Contest 2020."
-            }
-          />
-          <Achivement
-            Text={
-              "Champions, Mathophobic Skyvers, LU Intra University Programming Competition 2020."
-            }
-          />
-          <Achivement
-            Text={
-              "Champions, LUZeroIQ, LU Intra University Programming Competition 2021."
-            }
-          />
-          <Achivement
-            Text={"Top 6 in Bangladesh, LUZeroIQ, IEEE Extreme 14."}
-          />
-          <Achivement
-            Text={
-              "Top 10 In Country Wise Ranking, Codeforces Round 643 (Div. 2)."
-            }
-          />
-          <Achivement Text={"National ICT Quiz Champions 2018."} />
-          <Achivement Text={"LU Intra University ICT Quiz Champions 2018."} />
-          <Achivement Text={"LU Intra University ICT Quiz Champions 2019."} />
-          <Achivement Text={"LU Intra University ICT Quiz Champions 2020."} />
-          <Achivement Text={"LU Intra University ICT Quiz Champions 2021."} />
+          {renderAchievements(achievementsData)}
         </motion.div>
       </div>
     </section>
